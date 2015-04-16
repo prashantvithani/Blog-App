d88e320c5cdaa71a77c4f7b8ca1d38ae862ae0f9
Prashant Vithani <prashantvithani@gmail.com>
2015-04-16 17:05:19 +0530

    Add Bootstrap Gem and provide design to the forms, header, and footer.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
+  def dob=(date)
+    date = date.to_date if date.is_a?(String)
+    write_attribute(:dob, date)
+  end
 end
