dc9f9a92064e4b24890caf8c75e4b490499663a8
Prashant Vithani <prashantvithani@gmail.com>
2015-05-13 14:05:29 +0530

    Fix bug with API keys

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,11 @@ module BlogApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    api_keys = File.join(Rails.root, 'config', 'api_keys.yml')
+    CONFIG = HashWithIndifferentAccess.new(YAML::load(IO.read(api_keys)))[Rails.env]
+    CONFIG.each do |k,v|
+      ENV[k.upcase] ||= v
+    end
   end
 end
