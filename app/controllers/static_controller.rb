7bad473e344228422708e14235388dcd498569a5
Prashant Vithani <prashantvithani@gmail.com>
2015-05-14 21:16:18 +0530

    Add feature of Blog Sharing.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,9 +3,19 @@ class StaticController < ApplicationController
 		@posts = Post.order("created_at DESC").all.paginate(page: params[:page], per_page: 10)
 	end
 
-	def support
-		Contact.delay.send_mail(current_user)
+	def share
+		post = Post.find(params[:id])
+		user = post.user
+		@users = User.all_except([current_user, user])
+		emails = @users.each.map { |e| e.email  }
+		Delayed::Job.enqueue(MailJob.new(current_user, emails, user, post))
 		flash[:notice] = "Sending mail"
 		redirect_to root_url
+	end
+
+	def support
+		Contact.send_mail(current_user).deliver
+		flash[:notice] = "Mail sent. will get back to you shortly."
+		redirect_to :back
 	end
 end
