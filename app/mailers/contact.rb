31ff6fe2527caa0ed272ecdb11c0766dfa9e1b6f
Prashant Vithani <prashantvithani@gmail.com>
2015-05-14 21:17:47 +0530

    Remove Mailers

--- app/mailers/contact.rb
+++ app/mailers/contact.rb
@@ -2,7 +2,6 @@ class Contact < ActionMailer::Base
   default from: "contact@blogapp.com"
   def send_mail(user)
     @user = user
-    sleep 10
     mail(to: "prashantvithani@gmail.com", subject: "Complaints")
   end
 end
