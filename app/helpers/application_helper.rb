8ce400031bc5b6d6e78c9853c413b7762fee72f3
Prashant Vithani <prashantvithani@gmail.com>
2015-05-14 21:16:52 +0530

    Increase the size of profile picture on Blog Post detail.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,13 @@
 module ApplicationHelper
-	def gravatar_for(user, model = "profile", options = { size: 50 })
+	def gravatar_for(user, model = "header", options = { size: 50 })
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
 		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
 		if(model == "comment")
 			image_tag(gravatar_url, alt: user.full_name, class: "media-object img-circle")
 		elsif(model == "home")
 			image_tag(gravatar_url, alt: user.full_name, class: "media-object img-rounded")
+		elsif(model == "profile")
+			image_tag(gravatar_url, alt: user.full_name, class: "social-avatar img-rounded")
 		else
 			image_tag(gravatar_url, alt: user.full_name, class: "avatar img-circle")
 		end
