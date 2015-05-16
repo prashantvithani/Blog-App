379b5eea7e9f9c387a0ecb26dc82bc7207916ed3
Prashant Vithani <prashantvithani@gmail.com>
2015-05-16 13:33:07 +0530

    Mailer configuration.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,5 +38,14 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  # config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => "gmail.com",
+      :user_name            => ENV['gmail_username'],
+      :password             => ENV['gmail_password'],
+      :authentication       => :plain,
+      :enable_starttls_auto => true
+}
 end
